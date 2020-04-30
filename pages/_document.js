ac39bc410f726afa06ded22e5df28c735e8f2ae9
magus <noah@iamnoah.com>
2020-04-30 02:43:00 -0700

    apple touch icon white margin

--- pages/_document.js
+++ pages/_document.js
@@ -49,14 +49,6 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content="#ffffff" />
 
           {/* iOS */}
-          <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <meta name="apple-mobile-web-app-title" content="Catalog" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
