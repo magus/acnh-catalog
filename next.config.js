c570a41bb59f71604d95fd410f197fe71e2168e6
magus <noah@iamnoah.com>
2020-05-01 02:49:20 -0700

    service worker: cache villagerdb

--- next.config.js
+++ next.config.js
@@ -12,20 +12,34 @@ const nextConfig = {
   workboxOpts: {
     swDest: 'static/service-worker.js',
 
+    // Read more on available handlers
+    // https://developers.google.com/web/tools/workbox/modules/workbox-strategies
     runtimeCaching: [
       {
-        urlPattern: /.png$/,
+        // villagedb images from cache first
+        urlPattern: /^https?:\/\/villagerdb\.com\/.*\.png$/,
         handler: 'CacheFirst',
+        options: {
+          cacheName: 'villagerdb-images',
+          cacheableResponse: {
+            statuses: [0, 200, 304], // villagerdb responds with 304 on success
+          },
+          expiration: {
+            maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
+            purgeOnQuotaError: true,
+          },
+        },
       },
+      // non-villagedb image https calls
       {
-        urlPattern: /^https?.*/,
+        urlPattern: /^((?!villagerdb).)*$/,
         handler: 'NetworkFirst',
         options: {
           cacheName: 'https-calls',
           networkTimeoutSeconds: 15,
           expiration: {
-            maxEntries: 150,
             maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+            purgeOnQuotaError: true,
           },
           cacheableResponse: {
             statuses: [0, 200],
