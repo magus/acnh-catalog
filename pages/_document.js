d1a8cf5ccfa3c566ee347246f76a88bad4be177c
magus <noah@iamnoah.com>
2020-05-02 16:12:56 -0700

    _document: app header default

--- pages/_document.js
+++ pages/_document.js
@@ -53,7 +53,7 @@ export default class MyDocument extends Document {
           <meta name="apple-mobile-web-app-title" content="Catalog" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-title" content="Catalog" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <link
             rel="apple-touch-startup-image"
             href="/images/splash/launch-640x1136.png"
