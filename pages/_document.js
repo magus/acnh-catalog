dc727b56a496e6df187db16dc81c77c7516dfe99
magus <noah@iamnoah.com>
2020-05-02 18:14:41 -0700

    ssr styled components

--- pages/_document.js
+++ pages/_document.js
@@ -4,25 +4,30 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    // Render app and page and get the context of the page with collected side effects.
-    const sheets = new ServerStyleSheet();
+    // Render app and page and get the context of the page with collected side effects
+    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
-    ctx.renderPage = () => {
-      return originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collectStyles(<App {...props} />),
-      });
-    };
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
 
-    const initialProps = await Document.getInitialProps(ctx);
-
-    // Styles fragment is rendered after the app and page rendering finish.
-    const styles = [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()];
-
-    return {
-      ...initialProps,
-      styles,
-    };
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        // Styles fragment is rendered after the app and page rendering finish
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
