6bb96f77a9f6c2ab98a55d2be7a59e853bb9806e
magus <noah@iamnoah.com>
2020-05-12 00:57:20 -0700

    google analytics for web-vital metrics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,33 @@ import Head from 'next/head';
 import { createGlobalStyle } from 'styled-components';
 // import App from 'next/app'
 
+// Will be called once for every metric that has to be reported.
+// https://nextjs.org/blog/next-9-4#integrated-web-vitals-reporting
+export function reportWebVitals(metric) {
+  // These metrics can be sent to any analytics service
+  console.debug(metric);
+
+  // Assumes the global `gtag()` function exists, see:
+  // https://developers.google.com/analytics/devguides/collection/gtagjs
+  if (window.gtag) {
+    const { id, name, label, value } = metric;
+    window.gtag('event', name, {
+      event_category: `nextjs metric (${label})`,
+      // Google Analytics metrics must be integers, so the value is rounded.
+      // For CLS the value is first multiplied by 1000 for greater precision
+      // (note: increase the multiplier for greater precision if needed).
+      value: Math.round(name === 'CLS' ? value * 1000 : value),
+      // The `id` value will be unique to the current page load. When sending
+      // multiple values from the same page (e.g. for CLS), Google Analytics can
+      // compute a total by grouping on this ID (note: requires `eventLabel` to
+      // be a dimension in your report).
+      event_label: id,
+      // Use a non-interaction event to avoid affecting bounce rate.
+      non_interaction: true,
+    });
+  }
+}
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
