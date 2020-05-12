750da51405d9b43b344c33b7ad1d84e2a49f9dab
magus <noah@iamnoah.com>
2020-05-12 00:41:18 -0700

    wishlist icon path cleanup

--- src/components/icons/Wishlist.js
+++ src/components/icons/Wishlist.js
@@ -7,7 +7,7 @@ export default function Wishlist({ active, color }) {
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
       <path
         fill={fill}
-        d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm"
+        d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75z"
       />
     </svg>
   );
